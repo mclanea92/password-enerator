12c5d6baacc42a61b9f651a980f35741d094fd86
Adam McLane <mclanea92@gmail.com>
2022-03-20 12:48:47 -0400

    added length for password to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789"
+var symbols = "!@#$%^&*_-+="
+var upperLimit = 128;
+var lowerLimit = 8;
 
+// var passwordLength = {
+//   from: 8, 
+//   to: 128 
+// }
+//var writePassword = '';
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-
+  var event = prompt("How many characters are required in your password?");
+  if (event >= 8 && event < 129){
+    console.log("This password is the right length")}
   passwordText.value = password;
     
 }
 
+
+
 function generatePassword() {
-  var length = (8 to 128)
-  var string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
-  return ("gibberish")
+
+
+
+  return
 }
 
 // Add event listener to generate button
