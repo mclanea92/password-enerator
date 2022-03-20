d90cd36fd669e02634dd201c2b0d2ddbfd96ed57
Adam McLane <mclanea92@gmail.com>
2022-03-20 13:29:13 -0400

    updated javascript

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,10 @@ var generateBtn = document.querySelector("#generate");
 var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = "0123456789"
 var symbols = "!@#$%^&*_-+="
+var confirm = ["yes" , "YES" , "Yes" , "Y" , "yup" , "Yup" , "y"]
 var upperLimit = 128;
 var lowerLimit = 8;
+var hasSymbol = true;
 
 // var passwordLength = {
 //   from: 8, 
@@ -18,9 +20,49 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   var event = prompt("How many characters are required in your password?");
   if (event >= 8 && event < 129){
-    console.log("This password is the right length")}
-  passwordText.value = password;
-    
+    alert("this password is the right length")}
+    else {
+      alert("Please select a value between 8 and 128") //NEEDS TO RE-PROMPT USER TO SELECT CORRECT NUMBER
+    }
+  //NEED TO DO SOMETHING WITH THE VALUE THE USER GIVES US
+  var event = confirm("Press confirm if you would like special characters");
+  if (confirm == true) {
+    alert("You've selected special characters!")
+  } else if (confirm == false) { 
+    alert("You've selected not to have special characters :(")
+  }
+  var event = confirm("Press confirm if you would like uppercase letters.");
+  if (confirm == true) {
+    alert("You've selected uppercase letters!")
+  } else if (confirm == false) {
+    alert("You've selected not to have uppercase letters :(")
+  }
+  //NEED TO DO SOMETHING WITH THE INPUT
+  
+  // var event = prompt("How many characters are required in your password?");
+  // if (event >= 8 && event < 129){
+  //   alert("This password is the right length")}
+  // else {
+  //   alert("Please choose chararter value between 8 & 128.") // need to run something to reprompt user to select correct number if not in criteria
+  // }
+
+  
+  // var event = confirm("Are special characters required?");
+  // if (confirm == true){
+  //   alert("You have selected special characters")}
+  //   else if (confirm = false) {
+  //   alert("You have selected to not have special characters")
+  // }
+
+
+  // var event = confirm ("Press confirm if you would like uppercase letters");
+  // if (confirm = true){
+  //   alert("You have selected uppercase letters")}
+  //   else if (confirm = false) {
+  //     alert("You have selected to not have uppercase letters")
+  //   }
+
+    passwordText.value = password;
 }
 
 
