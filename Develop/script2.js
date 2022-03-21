faca0d631b3f839947d267146903f77d4f9e756f
Adam McLane <mclanea92@gmail.com>
2022-03-21 16:09:33 -0400

    updated javascript

--- Develop/script2.js
+++ Develop/script2.js
@@ -4,51 +4,51 @@
 function makePassword() {
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialchar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerletter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperletter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var passwordarray = [];
 
 var passwordLength = getPWLength();
 
 var charTypeSelected = false;
 while (charTypeSelected == false) {
-    var low = getChoice("lowercase");
-    var up = getChoice("uppercase");
-    var number = getChoice("numb");
-    var specialc = getChoice("special");
-    if ((lowercase) || (uppercase) || (number) || (special)) {
+    var low = getpass("LowerCase");
+    var up = getpass("UpperCase");
+    var number = getpass("Number");
+    var special = getpass("Special");
+    if ((low) || (up) || (number) || (special)) {
         charTypeSelected = true;
     }
     else {
-        window.alert("Please select at lease one type")
+        window.alert("Please select at lease one type.")
     }
 }
  if (low) {
-     passwordarray = passwordarray.concat(lower);
+     passwordarray = passwordarray.concat(lowerletter);
  }
  if (up) {
-     passwordarray = passwordarray.concat(upper);
+     passwordarray = passwordarray.concat(upperletter);
  }
  if (number) {
      passwordarray = passwordarray.concat(numbers);
  }
- if (specialc) {
+ if (special) {
      passwordarray = passwordarray.concat(specialchar);
  }
 
-var PWString = "";
-//loop
-for (var i=0; i < passwordLength; i++) {
-    PWString += passwordarray[Math.floor(math.random() * (passwordarray.length))];
-}
+ var PWString ="";
+ for (var i = 0; i < passwordLength; i++) {
+     PWString += passwordarray[Math.floor(Math.random() * (passwordarray.length))];
+ }
+
 
 return PWString;
 }
 
 function getPWLength() {
     var passlength = 0;
-    while ((passlength > 7) || (passlength < 129)) {
-        passlength = parseInt(window.prompt("How many characters do you want from 8 to 128"));
+    while ((passlength < 7) || (passlength > 129)) {
+        passlength = parseInt(window.prompt("How many characters do you want from (8 - 128)?: "));
         
         if (isNaN(passlength)) {
             passlength = 0;
@@ -58,19 +58,19 @@ function getPWLength() {
 }
 
 
-function getChoice(currentOption) {
-    var userChoice = "a",
+function getpass(currentOption) {
+    var passlength = "a",
       messagePrompt = "";
     var messagePrompt = ('Would you like '.concat(currentOption));
     messagePrompt = messagePrompt.concat(' characters (y/n)?');
     // This loop ensures the user enters a valid response.
-    while (userChoice = "a") {
-      userChoice = (window.prompt(messagePrompt));
+    while (passlength = "a") {
+      passlength = (window.prompt(messagePrompt));
       // Added the line below for ease of usability on mobile devices as some of them automatically capitalize when entering input.
-      userChoice = userChoice.toLowerCase();
-      if (userChoice == "y") {
+      passlength = passlength.toLowerCase();
+      if (passlength == "y") {
         return true;
-      } else if (userChoice == "n") {
+      } else if (passlength == "n") {
         return false;
       }
     }
